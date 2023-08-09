1e11665ba416b2df866ef224a1e44f0fcdd7a3e7
Vitor <vitorsantos4736@gmail.com>
2023-08-09 13:55:04 -0300

    Adição do alembic e iniciando tabela de dados, configurações etc

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
+app.config.from_pyfile(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../config.py'))
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
